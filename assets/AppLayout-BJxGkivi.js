5d97287c4b421fe26dc9f738bf3ca284236dcce1
Halifatick <Halifatick@users.noreply.github.com>
2025-03-13 23:20:15 +0000

    deploy: 0d3308d2b766ea1edd195dc3e7fd498f2ff50dc8

---
+++ assets/AppLayout-BJxGkivi.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./index-ugRx4O2W.js";import"./vue-MVS1_hXh.js";export{m as default};
---
+++ assets/AppLayoutDefault-RKi0fRji.js
@@ -0,0 +1,1 @@
+import{d as t,q as a,o,s,b as n}from"./vue-MVS1_hXh.js";const c={class:"flex flex-auto"},r={class:"container mx-auto"},p=t({__name:"AppLayoutDefault",setup(l){return(e,_)=>(o(),a("main",c,[s("div",r,[n(e.$slots,"default")])]))}});export{p as default};
