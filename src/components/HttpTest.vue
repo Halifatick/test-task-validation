bba4aeed842c3d88e673b9e6505a3348c45a9d7d
Артём Крыжевич <artem.kryzhevich.a@yandex.ru>
2025-03-14 02:37:08 +0300

    fix

--- src/components/HttpTest.vue
+++ src/components/HttpTest.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { useHttp } from '@/composables/useHttp'
 import Spinner from '@/assets/spinner.svg'
-const { data, loading, success, error, fetchData } = useHttp<{ message: string }>(
-    'https://dog.ceo/api/breeds/image/random',
+
+const getRandomId = () => Math.floor(Math.random() * 100) + 1;
+
+const randomPostUrl = `https://jsonplaceholder.typicode.com/posts/${getRandomId()}`;
+
+const { data, loading, success, error, fetchData } = useHttp<{ userId: number, id: number, title: string, body: string }>(
+    randomPostUrl,
     { method: 'GET' }
 );
 
@@ -14,7 +19,15 @@ fetchData();
   <div class="container">
     <Spinner v-if="loading" />
     <div v-else>
-      <img v-if="success" :src="data?.message">
+      <div v-if="success" class="card">
+        <div class="card-header">
+          <h2>{{ data.title }}</h2>
+          <p class="user-id">User ID: {{ data.userId }}</p>
+        </div>
+        <div class="card-body">
+          <p>{{ data.body }}</p>
+        </div>
+      </div>
       <div v-if="!!error" class="error">{{ error }}</div>
     </div>
   </div>
@@ -25,10 +38,46 @@ fetchData();
   display: flex;
   justify-content: center;
   align-items: center;
+  background-color: #f4f7fb;
+  padding: 20px;
 }
+
 .error {
   color: red;
   font-size: 16px;
   margin-top: 20px;
+}
+
+.card {
+  width: 100%;
+  max-width: 600px;
+  margin: 20px auto;
+  padding: 20px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.card-header {
+  border-bottom: 2px solid #f2f2f2;
+  padding-bottom: 10px;
+  margin-bottom: 10px;
+}
+
+.card-header h2 {
+  font-size: 1.5rem;
+  color: #333;
+  margin: 0;
+}
+
+.user-id {
+  font-size: 0.9rem;
+  color: #666;
+}
+
+.card-body p {
+  font-size: 1rem;
+  color: #444;
+  line-height: 1.6;
 }
 </style>
